791a3cf89506ea7e77c616876dc604c6b073e51f
sanchezd90 <sanchezd90@gmail.com>
2021-11-10 19:55:37 -0300

    agrego esquema de index

---
+++ index.js
@@ -0,0 +1,19 @@
+// const express = require ('express');
+// const app = express();
+//const connectDB = require('');
+
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+//connectDB();
+
+// app.use(express.json({ extended: true }));
+
+//const PORT = process.env.SERVER_PORT || 4000
+
+// app.use('/api/tests',require('./routes/tests'));
+
+// app.listen(PORT, () => {
+//     console.log(`Server escuchando en puerto ${PORT}`)
+// })
+
