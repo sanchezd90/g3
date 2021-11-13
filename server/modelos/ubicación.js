2f512963553867fde8b1c546f9e5aaee501677e3
MYRIAM ROQUE VASQUEZ <82110066+mroque71@users.noreply.github.com>
2021-11-13 16:38:05 -0600

    ubicacion.js
    se anexa esta colección

--- server/modelos/ubicación.js
+++ server/modelos/ubicación.js
@@ -1,1 +1,38 @@
+const mongoose = require('mongoose');
 
+const propiedadesSchema = mongoose.Schema({
+    barrio: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    direccion: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    estrato: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    zona: {
+        type: Number,
+        require: true,
+        trim: true
+    },
+    ciudad: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    
+    codigo_postal: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    
+});
+
+module.exports = mongoose.model('ubicacion', ubicacionesSchema);
