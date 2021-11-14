8db4fccf50c8bcc61cc059d99cef2e6a40e5e0a3
MYRIAM ROQUE VASQUEZ <82110066+mroque71@users.noreply.github.com>
2021-11-13 18:56:44 -0600

    Update main.js

--- server/controladores/main.js
+++ server/controladores/main.js
@@ -1,4 +1,4 @@
-var ngApp = angular.module('quickstartersApp', []);
+var ngApp = angular.module('QuickstartersApp', []);
   
  ngApp.controller('myController', function ($scope, $http) {
 
@@ -23,14 +23,8 @@ var ngApp = angular.module('quickstartersApp', []);
  onload = function () {
              document.getElementById('lbtnTest').onclick = function () {
                  alert("hola")
-      // Obtenga la aplicación Angular a través del controlador
                  var appElement = document.querySelector('[ng-controller=myController]');
-      // Obtenga la variable $ scope
+  
                 var $scope = angular.element(appElement).scope();
                  
-      // Llama a la variable msg y cambia el valor de msg
-                 $scope.msg = '123456';
-      // La línea anterior cambió el valor de msg. Si desea sincronizar con el controlador angular, debe llamar al método $ apply ().
-                 $scope.$apply();
-      // Llame al método getData () en el controlador
              console.log($scope.
