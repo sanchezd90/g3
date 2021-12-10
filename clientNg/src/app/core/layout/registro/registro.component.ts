229a171454c5b50f1659faaaa4baa2d4666a6e06
sanchezd90 <sanchezd90@gmail.com>
2021-12-10 10:59:04 -0300

    [dsr] edito formulario de registro de usuarios

--- clientNg/src/app/core/layout/registro/registro.component.ts
+++ clientNg/src/app/core/layout/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from '../../../service/usuarios.service';
+import { Usuario } from './usuario'
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:UsuariosService) { }
 
   ngOnInit(): void {
   }
+
+  model = new Usuario('','','');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  register(form:any): void {
+    const {user_id, password, email} = form.controls                
+  }
+
+  
 
 }
---
+++ clientNg/src/app/core/layout/registro/usuario.ts
@@ -0,0 +1,7 @@
+export class Usuario {
+    constructor(        
+        public user_id: string,
+        public password: string,
+        public email: string,                      
+    ) {}
+  }
